a2f39a70e76d38ec77130c430608b4e486141dac
Kishor <kishbuy238@gmail.com>
2017-11-13 18:38:13 -0500

    Working code for user_input string storage

--- prog.asm
+++ prog.asm
@@ -1,38 +1,21 @@
 .data 
-
-str:    
-    .space 9 #8 bits for 8 characters and 1 bit for end 
-str1:
-    asciiz "Invalid number" 
+    prompt: .asciiz "Enter a string:\n"
+    user_input:    .space 9 #max 8 characters + 1 end character
+    str1: .asciiz "Invalid number" 
     
 .text 
 
-li $v0, 8 #v0 code 8 means OS expects to input a string from user
-la $a0, str #address of str in memory store in register  $a0 
-li $a1, 9 #OS knows 9 bits string is about to get inputted 
-syscall #Call to the system 
+    li $v0, 4 #call code for printing out a string
+    la $a0, prompt #address of prompt put in argument register $a0
+    syscall $Displays the prompt 
+    
+    la $t0, user_input #address of user_inputs in memory store in register #$t0 now has the address of user_input 
+    
+    li $v0, 8 #v0 code 8 means OS expects to input a string from user
+    la $a0, $t0    #$a0 has value in $t0 
+    li $a1, 9 #OS knows max length of user input string
+    syscall #Call to the system 
 #By this point, user input string is stored in memory, and that address is in register $a0
 
-j slabel #jumps to slabel right away
-label: 
-    addi $a0, 1  
-slabel: 
-    lb $t0, 0($a0) #loads a byte from 0($a0) to register $t0
-    beq $t0, 8($a0) right_label #if $t0 has address of 8th byte, go to right_label, or move down
-    
-beq $t0, valid, label #checks if that byte has a valid number. if yes, goes to label to check another byte
 
-li $v0, 4  #if that corresponding byte in memory is invalid, $v0 has call code4 for printing out str1 
 
-la $a0, str1 #address of str1 is put into $a0
-syscall #takes argument from $a0
-li $v0, 10 #call code for exit
-exit  #program exits
-
-right_label:
-    convert to decimal #converting is done here
-    li $v0, 1 #call code for printing out an integer
-    li $a0, answer #register $a0 has value to be printed 
-    syscall #prints out argument $a0
-    li $v0, 10 #call code for exit
-    exit  #program exits
