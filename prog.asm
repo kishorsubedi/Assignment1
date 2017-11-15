e16800e914549f30e63bd0709b13cc7787a555e2
Kishor <kishbuy238@gmail.com>
2017-11-14 23:27:37 -0500

    Validated the input number and changed into hexadecimal or prompted invalid_number

--- prog.asm
+++ prog.asm
@@ -1,49 +1,81 @@
 .data 
-    prompt: .asciiz "Enter a string:\n"
-    user_input:    .space 8 #max 8 characters 
-    invalid_prompt: .asciiz "Invalid number" 
-    valid_prompt:   .asciiz "Valid hexadecimal number"
-.text 
+	user_input: .space 9		#allocate 9 bytes of storage for 8 charecters in user_input label
+	invalid_input:	.asciiz "\n Invalid hexadecimal number \n"
+	newline: .asciiz "\n"  		# Newline character stored in memory under label newline 
+	
+.text					#Beginning of instructions
 
-    li $v0, 4 #call code for printing out a string
-    la $a0, prompt #address of prompt put in argument register $a0
-    syscall #Displays the prompt 
-    
-    la $t0, user_input #address of user_inputs in memory store in register #$t0 now has the address of user_input 
-    
-    li $v0, 8 #v0 code 8 means OS expects to input a string from user
-    la $a0, ($t0)    #$a0 has value in $t0 
-    li $a1, 9 #OS knows max length of user input string
-    syscall #Call to the system 
-#By this point, user input string is stored in memory, and that address is in register $a0
-    addi $t7, $t0, 8 #$t7 holds the 9th byte from user_input address, to check all the characters have gone through validation
-    
-    target:
+main:
+	addi, $s3, $0, 0
+	li $v0, 8			#call code to read string
+	la $t0, user_input		#load address of user_input in register $t0
+	la $a0, ($t0)			#load address of user_input in $t0 to argument register $a0
+	la $a1, 9			#OS knows the max byte the user_input is going to be 
+	syscall				#syscall to read user_input and store the string in memory
 
-        lb $t1, 0($t0) #loaded 1st byte of memory into $t1
-        blt $t1, 48, invalid #branch to invalid label if $t1 is less than 48 
-        blt $t1, 58, valid
-        blt $t1, 65, invalid
-        blt $t1, 71, valid
-        blt $t1, 97, invalid
-        blt $t1, 103, valid
-        bgt $t1, 102, invalid
-    
-    invalid: #Prompts "Invalid Number" and program exits
-        li $v0, 4 
-        la $a0, invalid_prompt 
-        syscall 
-        li $v0, 10 
-        syscall 
-        
-    valid: #Prompts something and program exits 
-        addi $t0, $t0, 1 # $t0 is immediately increased by 1 
-        bne $t0, $t7, target #If $t0 != $t7, there's still more characters to be checked out.
-        
-        li $v0, 4 
-        la $a0, valid_prompt
-        syscall 
-        li $v0, 10 
-        syscall 
-    
-    
+	addi $t7, $t0, 8		#Address of 9th byte from $t0 is now stored into $t7
+	addi $s5, $t0, 0
+
+length_check:				#the length of the string in memory is checked in this label
+	lb $t1, 0($s5)
+	beq $t1, 0, sub_four
+	beq $t1, 10, sub_four
+	addi $s3, $s3, 4
+	addi $s5, $s5, 1 
+	j length_check
+sub_four:				#if value in first offset refers to new line or null, label to branch 
+	addi $s3, $s3, -4
+	
+iterate_string:				#iterate through user_input to check if all the charectars are valid	
+	
+	lb $t1, 0($t0)			#load first byte of memory into register $t1
+
+	beq $t1, 0, less_than_eight	#if the value at offset 0 < 8 or <10, branch to less_than 8
+	beq $t1, 10, less_than_eight
+	blt $t1, 48, invalid		#branch to if_invalid label if value in $t1 is less than 48 (ASCII dec for number 0)		
+	addi $s1, $0, 48		# store the ASCII dec to be subtracted in $s1
+	blt $t1, 58, valid		#branch to if_invalid label if value in $t1 is less than 58 (next ASCII dec for number 9)		
+	blt $t1, 65, invalid		# 65 = ASCII dec for 'A'
+	addi $s1, $0, 55
+	blt $t1, 71, valid		# 71 = ASCII dec for 'G' , user_input is valid up to 'F'
+	blt $t1, 97, invalid		# 97 = ASCII dec for 'a'
+	addi $s1, $0, 87
+	blt $t1, 103, valid	
+	bgt $t1, 102, invalid		#branch to if_invalid if value in $t1 is greater than 102 (ASCII dec for 'f')
+	
+invalid:				#label to call invalid_input and exit program
+	li $v0, 4			#call code to prompt invalid_input string
+	la $a0, invalid_input
+	syscall
+	li $v0, 10			#call code to exit program
+	syscall
+valid:					#label to call valid_input and exit program
+	addi $t0, $t0, 1		#increment offset of $t0 by 1
+	sub $s4, $t1, $s1		#subtract appropriate ASCII dec value to get dec value of hex input
+	sllv $s4, $s4, $s3		# multiply $s3 by 4
+	addi $s3, $s3, -4
+	add $s2, $s4, $s2		# add and store the value of $s4 and $s2 to $s2
+	bne $t0, $t7, iterate_string	# branch to iterate_string if offset of $t0 not equal to $t7
+
+less_than_eight:			#if the value at offset 0 < 8 branch to this label
+	addi $s0, $0, 10		#10 is stored in $s0
+	addi $t0, $t0, -1		#subtract 1 from user_input register $t0
+	lb $t1, 0($t0)
+	blt $t1, 56, exit_loop		#if 0($t0) < 8, branch to exit_loop
+	divu $s2, $s0			# otherwise divide by 10 and store in $s2
+	
+	li $v0, 4 
+	la $a0, newline 
+	syscall 
+	
+	mflo $a0			# move lower bits from register LO to $a0 and print
+	li $v0, 1			#call code to print integer 
+	syscall
+	mfhi $s2			#move higher bits of register HI to $s2
+	
+exit_loop:
+	li $v0, 1			#callcode to print integer
+	addi $a0, $s2, 0
+	syscall
+	li $v0, 10			#call code to exit program
+	syscall
